7cd846089ee30fa7323fe2edec602263961feffc
yiqsong727 <yiqsong@student.unimelb.edu.au>
2024-05-20 21:37:58 +1000

    Organize GitHub Repository
    dalete origin 'fission'

--- fission/function/route-test/route-test.py
+++
@@ -1,7 +0,0 @@
-from flask import request
-
-
-def main():
-    year = int(request.args.get('year'))
-    month = int(request.args.get('month'))
-    return f'the year is :{year}- and month is {month}'
